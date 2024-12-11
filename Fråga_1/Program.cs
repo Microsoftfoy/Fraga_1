463e3a5ceaae84b7b7b5b911626c88d814625c8d
ercr07001 <ercr07001@E-PW05CGR0>
2024-12-11 12:01:30 +0100

    Add project files.

---
+++ Fråga_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace F1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
